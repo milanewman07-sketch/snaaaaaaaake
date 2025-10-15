b4d9875c867f3138083c32ab97593a3ac4368679
milanewman07-sketch <milanewman07@gmail.com>
2025-10-15 11:57:26 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 const GRID_SIZE = 20;
 
 let died = false
-
-let up;
-let down;
-let left;
-let right;
+let start = true
+let lastInput = 0;
 
 let headX = 300;
 let headY = 300;
@@ -17,54 +14,86 @@ let foodY = 0;
 
 function setup() {
     createCanvas(600, 600)
-    frameRate(3)
+    frameRate(5)
 }
 
 function draw() {
     background(255)
 
-    if (keyCode == ENTER && died) {
+
+    //check if snake is in bounds
+    if (isOutOfBounds(headX, -20, width) || isOutOfBounds(headY, -20, height)) {
+        died = true
+    }
+
+    // if dead condition T enter died state
+    if (died) {
+        text("died", 500, 500)
+    }
+
+
+    if (keyCode == ENTER) {
         died = false
         headX = 300
         headY = 300
-    } else if (keyCode == UP_ARROW && !died) {
+        start = true
+    } else if (keyCode == UP_ARROW && !died && lastInput != 2) {
         moveY = -GRID_SIZE
         moveX = 0
-    } else if (keyCode == DOWN_ARROW && !died) {
+        lastInput = 1
+    } else if (keyCode == DOWN_ARROW && !died && lastInput != 1) {
         moveY = GRID_SIZE
         moveX = 0
-    } else if (keyCode == LEFT_ARROW && !died) {
+        lastInput = 2
+    } else if (keyCode == LEFT_ARROW && !died && lastInput != 4) {
         moveX = -GRID_SIZE
         moveY = 0
-    } else if (keyCode == RIGHT_ARROW && !died) {
+        lastInput = 3
+    } else if (keyCode == RIGHT_ARROW && !died && lastInput != 3) {
         moveX = GRID_SIZE
         moveY = 0
+        lastInput = 4
     } else {
-        moveX = 0
-        moveY = 0
+        if (lastInput === 1) {
+            moveY = -GRID_SIZE
+        } else if (lastInput === 2) {
+            moveY = GRID_SIZE
+        } else if (lastInput === 3) {
+            moveX = -GRID_SIZE
+        } else if (lastInput === 3) {
+            moveX = GRID_SIZE
+        }
     }
 
-    if (headX < 0) {
-        died = true
-        headX += GRID_SIZE
-    } else if (headX >= width) {
-        died = true
-        headX -= GRID_SIZE
-    } else if (headY < 0) {
-        died = true
-        headY += GRID_SIZE
-    } else if (headY >= height) {
-        died = true
-        headY -= GRID_SIZE
-    }
+
 
     fill(0);
     rect(headX, headY, 20)
     headX += moveX
     headY += moveY
 
+    if (start) {
+        foodX = floor(random(0,30) * 20)
+        foodY = floor(random(0,30) * 20)
+    }
 
+    makeFood()
+        
+
+    start = false
+}
+
+function makeFood() { 
     // food
     fill(255, 50, 0)
-    circle(foodX + 10, foodY + 10, 20)
+    circle(foodX + 5, foodY + 10, 20)
+}
+
+
+function isOutOfBounds(coordinate, minVal, maxVal) {
+    if (minVal < coordinate && coordinate < maxVal) {
+        return false
+    } else {
+        return true
+    }
 }
